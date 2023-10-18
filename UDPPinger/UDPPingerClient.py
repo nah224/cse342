051e5e8e6d19ae8741bd836445a7729fa53bf858
nah224 <nah224@lehigh.edu>
2023-10-17 20:01:51 -0400

    Initial UDPPinger commit

---
+++ UDPPinger/UDPPingerClient.py
@@ -0,0 +1,14 @@
+# UDPPingerClient.py
+
+# The client should send 10 pings to the server. Because UDP is an unreliable 
+# protocol, a packet sent from the client to the server may be lost in the network,
+# or vice versa. For this reason, the client cannot wait indefinitely for a reply
+# to a ping message. You should get the client to wait up to one second for a reply;
+# if no reply is received within one second, your client program should assume that
+# the packet was lost during transmission across the network. You will need to look
+# up the Python documentation to find out how to set the timeout value on a datagram socket.
+
+# (1) send the ping message using UDP (Note: Unlike TCP, you do not need to establish a connection first, since UDP is a connectionless protocol.) 
+# (2) print the response message from server, if any 
+# (3) calculate and print the round trip time (RTT), in seconds, of each packet, if server responds 
+# (4) otherwise, print “Request timed out” 
