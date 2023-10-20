5ed316b9fe8a5bc529a4071695836875de6b4d14
nah224 <nah224@lehigh.edu>
2023-10-19 22:26:42 -0400

    Add rtt and comments

--- UDPPinger/UDPPingerClient.py
+++ UDPPinger/UDPPingerClient.py
@@ -1,35 +1,33 @@
 # UDPPingerClient.py
-
-# The client should send 10 pings to the server. Because UDP is an unreliable 
-# protocol, a packet sent from the client to the server may be lost in the network,
-# or vice versa. For this reason, the client cannot wait indefinitely for a reply
-# to a ping message. You should get the client to wait up to one second for a reply;
-# if no reply is received within one second, your client program should assume that
-# the packet was lost during transmission across the network. You will need to look
-# up the Python documentation to find out how to set the timeout value on a datagram socket.
-
-# (1) send the ping message using UDP (Note: Unlike TCP, you do not need to establish a connection first, since UDP is a connectionless protocol.) 
-# (2) print the response message from server, if any 
-# (3) calculate and print the round trip time (RTT), in seconds, of each packet, if server responds 
-# (4) otherwise, print “Request timed out” 
-
 from socket import *
+from time import *
 
+# Store server address and port
 serverAddress = ('localhost', 4041)
-
+# Create client socket
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-
+# Set client timeout at 1 second
 clientSocket.settimeout(1.0)
 
+# Iterate for 10 pings
 for pingNumber in range(10):
+    # Ping message to be sent
     msg = f'Ping {pingNumber}'.encode()
-
+    # Store start time
+    start = time()
+    # Send message to server
     clientSocket.sendto(msg, serverAddress)
 
+    # Attempt to get response message from server
     try:
+        # Get response from server
         data, server = clientSocket.recvfrom(1024)
-
-        print(f'{data.decode()}')
+        # Store end time
+        end = time()
+        # Calculate round trip time
+        rtt = end - start
+        # Print response and rtt
+        print(f'Response from {server[0]}: {data.decode()} - RTT: {rtt}')
     except timeout:
         # No response recieved
         print('Request timed out')
